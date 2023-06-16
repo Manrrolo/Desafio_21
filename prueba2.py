37220d21c673c6add3dfccc63829ee049308d0eb
Manrrolo <manuel.sc@uc.cl>
2023-06-16 17:55:18 -0400

    Cambio pueda Escoger archivos existentes

--- prueba2.py
+++ prueba2.py
@@ -1,16 +1,34 @@
-import os
-import pickle
-import requests
-import tempfile
+'''
+requirements.txt file contents:
+
+langchain==0.0.154
+PyPDF2==3.0.1
+python-dotenv==1.0.0
+streamlit==1.18.1
+faiss-cpu==1.7.4
+streamlit-extras
+'''
+
+
+import streamlit as st
 from dotenv import load_dotenv
+import pickle
+import os
+import glob
+import pickle
+from PyPDF2 import PdfReader
+from streamlit_extras.add_vertical_space import add_vertical_space
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
-from langchain.chat_models import ChatOpenAI
-from llama_index import GPTVectorStoreIndex, SimpleDirectoryReader, LLMPredictor, ServiceContext
-from PyPDF2 import PdfReader
-import streamlit as st
-from streamlit_extras.add_vertical_space import add_vertical_space
+from langchain.llms import OpenAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain.callbacks import get_openai_callback
+import os
+import tempfile
+import requests
+from IPython.display import Audio, clear_output
+from elevenlabs import generate, play, set_api_key, voices, Models
 
 # Sidebar contents
 with st.sidebar:
@@ -26,75 +44,112 @@ with st.sidebar:
     add_vertical_space(5)
     st.write('Made with ❤️ by [Prompt Engineer](https://youtube.com/@engineerprompt)')
     add_vertical_space(5)
-    st.write('Modified with 💪 by [Manrrolo](https://manrrolo.github.io/Page/)')
+    st.markdown('Modified with 💪 by [Manrrolo](https://manrrolo.github.io/Page/)')
+
 
 load_dotenv()
 
-# Replace with your OpenAI API key
-os.environ["OPENAI_API_KEY"] = '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'
-eleven_api_key = "880ec17adcf3ee790a09c2ef884a6e8c"
-selected_voice_id = "2"  # Antoni voice
+# Define las funciones para generar la respuesta de voz
+def get_voice_audio(text, voice_id="ErXwobaYiN019PkySvjV"):
+    CHUNK_SIZE = 1024
+    url = f"https://api.elevenlabs.io/v1/text-to-speech/{voice_id}"
+
+    headers = {
+      "Accept": "audio/mpeg",
+      "Content-Type": "application/json",
+      "xi-api-key": "3a3173ef3c657dc9add22b3a77b3708f"
+    }
+
+    data = {
+      "text": text,
+      "model_id" : "eleven_multilingual_v1",
+      "voice_settings": {
+        "stability": 0.4,
+        "similarity_boost": 1.0
+      }
+    }
+
+    response = requests.post(url, json=data, headers=headers)
+
+    # Save audio data to a temporary file
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".mp3") as f:
+        for chunk in response.iter_content(chunk_size=CHUNK_SIZE):
+            if chunk:
+                f.write(chunk)
+        f.flush()
+        temp_filename = f.name
+
+    return temp_filename
 
 def main():
     st.header("Chat with PDF 💬")
-    pdf = st.file_uploader("Upload your PDF", type='pdf')
 
-    if pdf is not None:
-        pdf_reader = PdfReader(pdf)
-        
-        text = ""
-        for page in pdf_reader.pages:
-            text += page.extract_text()
+    # Load all existing .pkl files
+    vector_stores = {}
+    for file in glob.glob("*.pkl"):
+        with open(file, "rb") as f:
+            vector_stores[file[:-4]] = pickle.load(f)
 
-        text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200, length_function=len)
-        chunks = text_splitter.split_text(text=text)
+    choice = st.radio("Do you want to:", ('Upload a new PDF', 'Use an existing .pkl file'))
 
-        store_name = pdf.name[:-4]
+    if choice == 'Upload a new PDF':
+        # upload a PDF file
+        pdf = st.file_uploader("Upload your PDF", type='pdf')
 
-        if os.path.exists(f"{store_name}.pkl"):
-            with open(f"{store_name}.pkl", "rb") as f:
-                VectorStore = pickle.load(f)
+        if pdf is not None:
+            with st.spinner('Cargando y procesando el PDF...'):
+                pdf_reader = PdfReader(pdf)
+                
+                text = ""
+                for page in pdf_reader.pages:
+                    text += page.extract_text()
+
+                text_splitter = RecursiveCharacterTextSplitter(
+                    chunk_size=1000,
+                    chunk_overlap=200,
+                    length_function=len
+                    )
+                chunks = text_splitter.split_text(text=text)
+
+                store_name = pdf.name[:-4]
+                
+                if os.path.exists(f"{store_name}.pkl"):
+                    with open(f"{store_name}.pkl", "rb") as f:
+                        VectorStore = pickle.load(f)
+                else:
+                    embeddings = OpenAIEmbeddings()
+                    VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
+                    with open(f"{store_name}.pkl", "wb") as f:
+                        pickle.dump(VectorStore, f)
+
+                # Add the new or updated VectorStore to the dictionary
+                vector_stores[store_name] = VectorStore
+
+    elif choice == 'Use an existing .pkl file':
+        if vector_stores:
+            selected_store = st.selectbox('Select a preprocessed PDF:', list(vector_stores.keys()))
+            VectorStore = vector_stores[selected_store]
         else:
-            embeddings = OpenAIEmbeddings()
-            VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
-            with open(f"{store_name}.pkl", "wb") as f:
-                pickle.dump(VectorStore, f)
+            st.write("No se encontraron archivos .pkl preprocesados.")
+            return
 
-        query = st.text_input("Ask questions about your PDF file:")
+    # Accept user questions/query
+    query = st.text_input("Ask questions about your PDF file:")
 
-        if query:
-            modelo = LLMPredictor(llm=ChatOpenAI(temperature=0, model_name='gpt-3.5-turbo'))
-            service_context = ServiceContext.from_defaults(llm_predictor=modelo)
-            index = GPTVectorStoreIndex.from_documents(VectorStore, service_context = service_context)
+    if query:
+        with st.spinner('Obteniendo la respuesta...'):
+            docs = VectorStore.similarity_search(query=query, k=3)
+
+            llm = OpenAI(temperature=0, model_name='gpt-3.5-turbo')
+            chain = load_qa_chain(llm=llm, chain_type="stuff")
+            with get_openai_callback() as cb:
+                response = chain.run(input_documents=docs, question=query+ " Responde en español")
+                print(cb)
+            # Convert the response to audio
+            audio_file = get_voice_audio(response)
+            st.audio(audio_file)
+            st.write(response)
             
-            respuesta = index.as_query_engine().query(query + " Responde en español")
-            st.write(respuesta.response)
-
-            url = "https://api.elevenlabs.io/v1/text-to-speech/" + selected_voice_id
-            headers = {
-                "Accept": "audio/mpeg",
-                "Content-Type": "application/json",
-                "xi-api-key": eleven_api_key
-            }
-            data = {
-                "text": respuesta.response,
-                "model_id" : "eleven_multilingual_v1",
-                "voice_settings": {
-                    "stability": 0.4,
-                    "similarity_boost": 1.0
-                }
-            }
-
-            response = requests.post(url, json=data, headers=headers)
-            CHUNK_SIZE = 1024
-            with tempfile.NamedTemporaryFile(delete=False, suffix=".mp3") as f:
-                for chunk in response.iter_content(chunk_size=CHUNK_SIZE):
-                    if chunk:
-                        f.write(chunk)
-                f.flush()
-                temp_filename = f.name
-
-            st.audio(temp_filename, format='audio/mp3')
 
 if __name__ == '__main__':
     main()
